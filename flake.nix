386a7c39d0115e4fb994c8f2911d2b4fc94b1b63
WuX <wux@thenest.dev>
2024-07-06 17:42:30 +0300

    tooling: prefer nix flakes over the shell.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+            inherit (pkgs) lib stdenv mkShell fetchFromGitLab buildGoModule;
+        in {
+            devShells.default = mkShell {
+                shellHook = "export DEVSHELL_PS1_PREFIX='charon'";
+                nativeBuildInputs = with pkgs; [
+                    pcre2
+                    llvmPackages_18.libllvm
+                    gcc14
+                    gnumake
+                ];
+            };
+        }
+    );
+}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-    buildInputs = with pkgs; [
-        pcre2
-        llvmPackages_18.libllvm
-        gcc13
-        gnumake
-    ];
-}
