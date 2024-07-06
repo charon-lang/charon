b83c6a842129dccdf188d8a07142deeeeb3b9db7
WuX <wux@thenest.dev>
2024-07-06 19:56:02 +0300

    util: make text from token

--- src/parser/util.c
+++ src/parser/util.c
@@ -16,3 +16,14 @@ token_t util_consume(tokenizer_t *tokenizer, token_kind_t kind) {
     if(token.kind == kind) return token;
     diag_error(UTIL_SRCLOC(tokenizer, token), "expected %s got %s", token_kind_tostring(kind), token_kind_tostring(token.kind));
 }
+
+static const char *util_text_make_from_token(tokenizer_t *tokenizer, token_t token) {
+    char *text = malloc(token.size + 1);
+    memcpy(text, tokenizer->source->data_buffer + token.offset, token.size);
+    text[token.size] = '\0';
+    return text;
+}
+
+static void util_text_free(tokenizer_t *tokenizer, const char *text) {
+    free((char *) text);
+}
