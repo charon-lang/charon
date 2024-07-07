1a42c1b3d1485f6019e7a74201209ab13c0ecd8e
WuX <wux@thenest.dev>
2024-07-07 13:16:45 +0300

    ir: added expression nodes and node list

--- src/ir/node.c
+++ src/ir/node.c
@@ -10,21 +10,66 @@ static ir_node_t *make_node(ir_node_type_t type, source_location_t source_locati
     return node;
 }
 
+void ir_node_list_append(ir_node_list_t *list, ir_node_t *node) {
+    if(list->first == NULL) list->first = node;
+    if(list->last != NULL) list->last->next = node;
+    list->last = node;
+}
+
 ir_node_t *ir_node_make_root(ir_node_list_t tlc_nodes, source_location_t source_location) {
     ir_node_t *node = make_node(IR_NODE_TYPE_ROOT, source_location);
     node->root.tlc_nodes = tlc_nodes;
     return node;
 }
 
-ir_node_t *ir_node_make_tlc_function(const char *name, source_location_t source_location) {
+ir_node_t *ir_node_make_tlc_function(const char *name, ir_node_list_t statements, source_location_t source_location) {
     ir_node_t *node = make_node(IR_NODE_TYPE_TLC_FUNCTION, source_location);
     node->tlc_function.name = name;
+    node->tlc_function.statements = statements;
     return node;
 }
 
-void ir_node_list_append(ir_node_list_t *list, ir_node_t *node) {
-    if(list->first == NULL) list->first = node;
-    node->next = list->last;
-    list->last = node;
-    list->count++;
+ir_node_t *ir_node_make_stmt_expression(ir_node_t *expression, source_location_t source_location) {
+    ir_node_t *node = make_node(IR_NODE_TYPE_STMT_EXPRESSION, source_location);
+    node->stmt_expression.expression = expression;
+    return node;
+}
+
+ir_node_t *ir_node_make_expr_literal_numeric(uintmax_t value, source_location_t source_location) {
+    ir_node_t *node = make_node(IR_NODE_TYPE_EXPR_LITERAL_NUMERIC, source_location);
+    node->expr_literal.numeric_value = value;
+    return node;
+}
+
+ir_node_t *ir_node_make_expr_literal_string(const char *value, source_location_t source_location) {
+    ir_node_t *node = make_node(IR_NODE_TYPE_EXPR_LITERAL_STRING, source_location);
+    node->expr_literal.string_value = value;
+    return node;
+}
+
+ir_node_t *ir_node_make_expr_literal_char(char value, source_location_t source_location) {
+    ir_node_t *node = make_node(IR_NODE_TYPE_EXPR_LITERAL_CHAR, source_location);
+    node->expr_literal.char_value = value;
+    return node;
+}
+
+ir_node_t *ir_node_make_expr_literal_bool(bool value, source_location_t source_location) {
+    ir_node_t *node = make_node(IR_NODE_TYPE_EXPR_LITERAL_BOOL, source_location);
+    node->expr_literal.bool_value = value;
+    return node;
+}
+
+ir_node_t *ir_node_make_expr_binary(ir_node_binary_operation_t operation, ir_node_t *left, ir_node_t *right, source_location_t source_location) {
+    ir_node_t *node = make_node(IR_NODE_TYPE_EXPR_BINARY, source_location);
+    node->expr_binary.operation = operation;
+    node->expr_binary.left = left;
+    node->expr_binary.right = right;
+    return node;
+}
+
+ir_node_t *ir_node_make_expr_unary(ir_node_unary_operation_t operation, ir_node_t *operand, source_location_t source_location) {
+    ir_node_t *node = make_node(IR_NODE_TYPE_EXPR_UNARY, source_location);
+    node->expr_unary.operation = operation;
+    node->expr_unary.operand = operand;
+    return node;
 }
--- src/ir/node.h
+++ src/ir/node.h
@@ -2,13 +2,46 @@
 
 #include "lib/source.h"
 
+#include <stdint.h>
+
 #define IR_NODE_LIST_INIT ((ir_node_list_t) { .count = 0, .first = NULL, .last = NULL })
 
 typedef enum {
     IR_NODE_TYPE_ROOT,
 
-    IR_NODE_TYPE_TLC_FUNCTION
+    IR_NODE_TYPE_TLC_FUNCTION,
+
+    IR_NODE_TYPE_STMT_EXPRESSION,
+
+    IR_NODE_TYPE_EXPR_LITERAL_NUMERIC,
+    IR_NODE_TYPE_EXPR_LITERAL_STRING,
+    IR_NODE_TYPE_EXPR_LITERAL_CHAR,
+    IR_NODE_TYPE_EXPR_LITERAL_BOOL,
+    IR_NODE_TYPE_EXPR_BINARY,
+    IR_NODE_TYPE_EXPR_UNARY
 } ir_node_type_t;
+
+typedef enum {
+    IR_NODE_BINARY_OPERATION_ADDITION,
+    IR_NODE_BINARY_OPERATION_SUBTRACTION,
+    IR_NODE_BINARY_OPERATION_MULTIPLICATION,
+    IR_NODE_BINARY_OPERATION_DIVISION,
+    IR_NODE_BINARY_OPERATION_MODULO,
+    IR_NODE_BINARY_OPERATION_GREATER,
+    IR_NODE_BINARY_OPERATION_GREATER_EQUAL,
+    IR_NODE_BINARY_OPERATION_LESS,
+    IR_NODE_BINARY_OPERATION_LESS_EQUAL,
+    IR_NODE_BINARY_OPERATION_EQUAL,
+    IR_NODE_BINARY_OPERATION_NOT_EQUAL,
+    IR_NODE_BINARY_OPERATION_ASSIGN
+} ir_node_binary_operation_t;
+
+typedef enum {
+    IR_NODE_UNARY_OPERATION_NOT,
+    IR_NODE_UNARY_OPERATION_NEGATIVE,
+    IR_NODE_UNARY_OPERATION_DEREF,
+    IR_NODE_UNARY_OPERATION_REF
+} ir_node_unary_operation_t;
 
 typedef struct ir_node ir_node_t;
 
@@ -27,13 +60,42 @@ struct ir_node {
 
         struct {
             const char *name;
+            ir_node_list_t statements;
         } tlc_function;
+
+        struct {
+            ir_node_t *expression;
+        } stmt_expression;
+
+        union {
+            uintmax_t numeric_value;
+            const char *string_value;
+            char char_value;
+            bool bool_value;
+        } expr_literal;
+        struct {
+            ir_node_binary_operation_t operation;
+            ir_node_t *left, *right;
+        } expr_binary;
+        struct {
+            ir_node_unary_operation_t operation;
+            ir_node_t *operand;
+        } expr_unary;
     };
     struct ir_node *next;
 };
 
+void ir_node_list_append(ir_node_list_t *list, ir_node_t *node);
+
 ir_node_t *ir_node_make_root(ir_node_list_t tlc_nodes, source_location_t source_location);
 
-ir_node_t *ir_node_make_tlc_function(const char *name, source_location_t source_location);
+ir_node_t *ir_node_make_tlc_function(const char *name, ir_node_list_t statements, source_location_t source_location);
 
-void ir_node_list_append(ir_node_list_t *list, ir_node_t *node);
+ir_node_t *ir_node_make_stmt_expression(ir_node_t *expression, source_location_t source_location);
+
+ir_node_t *ir_node_make_expr_literal_numeric(uintmax_t value, source_location_t source_location);
+ir_node_t *ir_node_make_expr_literal_string(const char *value, source_location_t source_location);
+ir_node_t *ir_node_make_expr_literal_char(char value, source_location_t source_location);
+ir_node_t *ir_node_make_expr_literal_bool(bool value, source_location_t source_location);
+ir_node_t *ir_node_make_expr_binary(ir_node_binary_operation_t operation, ir_node_t *left, ir_node_t *right, source_location_t source_location);
+ir_node_t *ir_node_make_expr_unary(ir_node_unary_operation_t operation, ir_node_t *operand, source_location_t source_location);
