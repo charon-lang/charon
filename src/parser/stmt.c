69091e3eb469b8d3028261474ee0754cfced5039
WuX <wux@thenest.dev>
2024-07-08 17:22:22 +0300

    parser: parse types for declarations

--- src/parser/stmt.c
+++ src/parser/stmt.c
@@ -2,6 +2,23 @@
 
 #include "parser/util.h"
 
+static ir_type_t *parse_type(tokenizer_t *tokenizer) {
+    token_t token_primitive_type = util_consume(tokenizer, TOKEN_KIND_IDENTIFIER);
+    if(util_token_cmp(tokenizer, token_primitive_type, "bool") == 0) return ir_type_get_bool();
+    if(util_token_cmp(tokenizer, token_primitive_type, "char") == 0) return ir_type_get_char();
+    if(util_token_cmp(tokenizer, token_primitive_type, "uint") == 0) return ir_type_get_uint();
+    if(util_token_cmp(tokenizer, token_primitive_type, "u8") == 0) return ir_type_get_u8();
+    if(util_token_cmp(tokenizer, token_primitive_type, "u16") == 0) return ir_type_get_u16();
+    if(util_token_cmp(tokenizer, token_primitive_type, "u32") == 0) return ir_type_get_u32();
+    if(util_token_cmp(tokenizer, token_primitive_type, "u64") == 0) return ir_type_get_u64();
+    if(util_token_cmp(tokenizer, token_primitive_type, "int") == 0) return ir_type_get_int();
+    if(util_token_cmp(tokenizer, token_primitive_type, "i8") == 0) return ir_type_get_i8();
+    if(util_token_cmp(tokenizer, token_primitive_type, "i16") == 0) return ir_type_get_i16();
+    if(util_token_cmp(tokenizer, token_primitive_type, "i32") == 0) return ir_type_get_i32();
+    if(util_token_cmp(tokenizer, token_primitive_type, "i64") == 0) return ir_type_get_i64();
+    return NULL;
+}
+
 static ir_node_t *parse_expression(tokenizer_t *tokenizer) {
     ir_node_t *expression = parser_expr(tokenizer);
     return ir_node_make_stmt_expression(expression, expression->source_location);
@@ -10,9 +27,11 @@ static ir_node_t *parse_expression(tokenizer_t *tokenizer) {
 static ir_node_t *parse_declaration(tokenizer_t *tokenizer) {
     source_location_t source_location = UTIL_SRCLOC(tokenizer, util_consume(tokenizer, TOKEN_KIND_KEYWORD_LET));
     token_t token_name = util_consume(tokenizer, TOKEN_KIND_IDENTIFIER);
+    ir_type_t *type = NULL;
+    if(util_try_consume(tokenizer, TOKEN_KIND_COLON)) type = parse_type(tokenizer);
     ir_node_t *initial = NULL;
     if(util_try_consume(tokenizer, TOKEN_KIND_EQUAL)) initial = parser_expr(tokenizer);
-    return ir_node_make_stmt_declaration(util_text_make_from_token(tokenizer, token_name), initial, source_location);
+    return ir_node_make_stmt_declaration(util_text_make_from_token(tokenizer, token_name), type, initial, source_location);
 }
 
 static ir_node_t *parse_simple(tokenizer_t *tokenizer) {
