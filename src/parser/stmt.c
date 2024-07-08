a138d730be3626bd3d32a719a230bd109c4d6aec
WuX <wux@thenest.dev>
2024-07-08 16:38:24 +0300

    parser: parse declarations

--- src/parser/stmt.c
+++ src/parser/stmt.c
@@ -7,8 +7,20 @@ static ir_node_t *parse_expression(tokenizer_t *tokenizer) {
     return ir_node_make_stmt_expression(expression, expression->source_location);
 }
 
+static ir_node_t *parse_declaration(tokenizer_t *tokenizer) {
+    source_location_t source_location = UTIL_SRCLOC(tokenizer, util_consume(tokenizer, TOKEN_KIND_KEYWORD_LET));
+    token_t token_name = util_consume(tokenizer, TOKEN_KIND_IDENTIFIER);
+    ir_node_t *initial = NULL;
+    if(util_try_consume(tokenizer, TOKEN_KIND_EQUAL)) initial = parse_expression(tokenizer);
+    return ir_node_make_stmt_declaration(util_text_make_from_token(tokenizer, token_name), initial, source_location);
+}
+
 static ir_node_t *parse_simple(tokenizer_t *tokenizer) {
-    ir_node_t *node = parse_expression(tokenizer);
+    ir_node_t *node;
+    switch(tokenizer_peek(tokenizer).kind) {
+        case TOKEN_KIND_KEYWORD_LET: node = parse_declaration(tokenizer); break;
+        default: node = parse_expression(tokenizer); break;
+    }
     util_consume(tokenizer, TOKEN_KIND_SEMI_COLON);
     return node;
 }
@@ -21,8 +33,7 @@ static ir_node_t *parse_block(tokenizer_t *tokenizer) {
 }
 
 ir_node_t *parser_stmt(tokenizer_t *tokenizer) {
-    token_t token = tokenizer_peek(tokenizer);
-    switch(token.kind) {
+    switch(tokenizer_peek(tokenizer).kind) {
         case TOKEN_KIND_BRACE_LEFT: return parse_block(tokenizer);
         default: return parse_simple(tokenizer);
     }
