2b167434f54e8b101a633154e44cd7d4160ab856
WuX <wux@thenest.dev>
2024-07-14 21:04:04 +0300

    ir: simplify function prototype

--- src/ir/function.c
+++ src/ir/function.c
@@ -2,10 +2,10 @@
 
 #include <stdlib.h>
 
-ir_function_t *ir_function_make(const char *name, ir_type_t *return_type) {
+ir_function_t *ir_function_make(const char *name) {
     ir_function_t *function = malloc(sizeof(ir_function_t));
     function->name = name;
-    function->return_type = return_type;
+    function->return_type = NULL;
     function->argument_count = 0;
     function->arguments = NULL;
     function->varargs = false;
@@ -15,8 +15,4 @@ ir_function_t *ir_function_make(const char *name, ir_type_t *return_type) {
 void ir_function_add_argument(ir_function_t *function, const char *name, ir_type_t *type) {
     function->arguments = realloc(function->arguments, ++function->argument_count * sizeof(ir_function_argument_t));
     function->arguments[function->argument_count - 1] = (ir_function_argument_t) { .name = name, .type = type };
-}
-
-void ir_function_set_varargs(ir_function_t *function, bool is_varargs) {
-    function->varargs = is_varargs;
 }
--- src/ir/function.h
+++ src/ir/function.h
@@ -9,12 +9,11 @@ typedef struct {
 
 typedef struct {
     const char *name;
-    ir_type_t *return_type;
+    ir_type_t *return_type; // OPTIONAL
     size_t argument_count;
     ir_function_argument_t *arguments;
     bool varargs;
 } ir_function_t;
 
-ir_function_t *ir_function_make(const char *name, ir_type_t *return_type);
+ir_function_t *ir_function_make(const char *name);
 void ir_function_add_argument(ir_function_t *function, const char *name, ir_type_t *type);
-void ir_function_set_varargs(ir_function_t *function, bool is_varargs);
