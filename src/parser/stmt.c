d4af9ad1939033f87845f9d11863564f4bb9b76e
WuX <wux@thenest.dev>
2024-08-06 02:11:10 +0300

    parser: add parsing for while statements

--- src/parser/stmt.c
+++ src/parser/stmt.c
@@ -35,6 +35,18 @@ static ir_node_t *parse_if(tokenizer_t *tokenizer) {
     return ir_node_make_stmt_if(condition, body, else_body, util_loc(tokenizer, token_if));
 }
 
+static ir_node_t *parse_while(tokenizer_t *tokenizer) {
+    token_t token_while = util_consume(tokenizer, TOKEN_KIND_KEYWORD_WHILE);
+    ir_node_t *condition = NULL;
+    if(util_try_consume(tokenizer, TOKEN_KIND_PARENTHESES_LEFT)) {
+        condition = parser_expr(tokenizer);
+        util_consume(tokenizer, TOKEN_KIND_PARENTHESES_RIGHT);
+    }
+    ir_node_t *body = parser_stmt(tokenizer);
+    return ir_node_make_stmt_while(condition, body, util_loc(tokenizer, token_while));
+}
+
+
 static ir_node_t *parse_simple(tokenizer_t *tokenizer) {
     ir_node_t *node;
     switch(tokenizer_peek(tokenizer).kind) {
@@ -56,6 +68,7 @@ static ir_node_t *parse_block(tokenizer_t *tokenizer) {
 ir_node_t *parser_stmt(tokenizer_t *tokenizer) {
     switch(tokenizer_peek(tokenizer).kind) {
         case TOKEN_KIND_KEYWORD_IF: return parse_if(tokenizer);
+        case TOKEN_KIND_KEYWORD_WHILE: return parse_while(tokenizer);
         case TOKEN_KIND_BRACE_LEFT: return parse_block(tokenizer);
         default: return parse_simple(tokenizer);
     }
