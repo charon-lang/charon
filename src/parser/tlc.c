d5a01bdf0ed33c87af4e8d2de5d4f0db9d9b578f
WuX <wux@thenest.dev>
2024-08-09 20:38:27 +0300

    parser: parse externs

--- src/parser/tlc.c
+++ src/parser/tlc.c
@@ -6,8 +6,8 @@
 
 #include <stdlib.h>
 
-static ir_node_t *parse_function(tokenizer_t *tokenizer) {
-    source_location_t source_location = util_loc(tokenizer, util_consume(tokenizer, TOKEN_KIND_KEYWORD_FUNCTION));
+static ir_function_t *parse_prototype(tokenizer_t *tokenizer) {
+    util_consume(tokenizer, TOKEN_KIND_KEYWORD_FUNCTION);
 
     ir_function_t *prototype = ir_function_make(util_text_make_from_token(tokenizer, util_consume(tokenizer, TOKEN_KIND_IDENTIFIER)));
     util_consume(tokenizer, TOKEN_KIND_PARENTHESES_LEFT);
@@ -26,11 +26,19 @@ static ir_node_t *parse_function(tokenizer_t *tokenizer) {
         } while(util_try_consume(tokenizer, TOKEN_KIND_COMMA));
     }
     util_consume(tokenizer, TOKEN_KIND_PARENTHESES_RIGHT);
+
     if(util_try_consume(tokenizer, TOKEN_KIND_COLON)) {
         prototype->return_type = util_parse_type(tokenizer);
     } else {
         prototype->return_type = ir_type_get_void();
     }
+
+    return prototype;
+}
+
+static ir_node_t *parse_function(tokenizer_t *tokenizer) {
+    source_location_t source_location = util_loc(tokenizer, tokenizer_peek(tokenizer));
+    ir_function_t *prototype = parse_prototype(tokenizer);
 
     ir_node_list_t statements = IR_NODE_LIST_INIT;
     util_consume(tokenizer, TOKEN_KIND_BRACE_LEFT);
@@ -38,10 +46,18 @@ static ir_node_t *parse_function(tokenizer_t *tokenizer) {
     return ir_node_make_tlc_function(prototype, statements, source_location);
 }
 
+ir_node_t *parse_extern(tokenizer_t *tokenizer) {
+    source_location_t source_location = util_loc(tokenizer, util_consume(tokenizer, TOKEN_KIND_KEYWORD_EXTERN));
+    ir_function_t *prototype = parse_prototype(tokenizer);
+    util_consume(tokenizer, TOKEN_KIND_SEMI_COLON);
+    return ir_node_make_tlc_extern(prototype, source_location);
+}
+
 ir_node_t *parser_tlc(tokenizer_t *tokenizer) {
     token_t token = tokenizer_peek(tokenizer);
     switch(token.kind) {
         case TOKEN_KIND_KEYWORD_FUNCTION: return parse_function(tokenizer);
+        case TOKEN_KIND_KEYWORD_EXTERN: return parse_extern(tokenizer);
         default: diag_error(util_loc(tokenizer, token), "expected top level construct, got %s", token_kind_tostring(token.kind));
     }
     __builtin_unreachable();
