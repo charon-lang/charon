3dc26b5648b89163fbb06a1afd33c67e9c5241ea
WuX <wux@thenest.dev>
2024-10-30 18:59:26 +0200

    tools: add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+            inherit (pkgs) lib stdenv mkShell fetchFromGitLab buildGoModule;
+        in {
+            devShells.default = mkShell {
+                shellHook = "export DEVSHELL_PS1_PREFIX='charon'";
+                nativeBuildInputs = with pkgs; [
+                    pcre2
+                    llvmPackages_18.libllvm
+                    lld_18
+                    clang_18
+                    clang-tools_18
+                    gnumake
+                    gdb
+                    bear
+                    gcc14
+                ];
+            };
+        }
+    );
+}
