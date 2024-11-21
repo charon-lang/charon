f1137f32d733b656166375a5e30a3f6fce65bd77
WuX <wux@thenest.dev>
2024-11-21 02:47:38 +0200

    hlir: add link attribute

--- src/hlir/attribute.c
+++ src/hlir/attribute.c
@@ -2,14 +2,16 @@
 
 #include <stdlib.h>
 
-static void list_add(hlir_attribute_list_t *list, hlir_attribute_kind_t kind, source_location_t source_location) {
+static hlir_attribute_t *list_add(hlir_attribute_list_t *list, hlir_attribute_kind_t kind, source_location_t source_location) {
     list->attributes = reallocarray(list->attributes, ++list->attribute_count, sizeof(hlir_attribute_t));
     list->attributes[list->attribute_count - 1] = (hlir_attribute_t) { .kind = kind, .source_location = source_location, .consumed = false };
+    return &list->attributes[list->attribute_count - 1];
 }
 
 hlir_attribute_t *hlir_attribute_find(hlir_attribute_list_t *list, hlir_attribute_kind_t kind) {
     for(size_t i = 0; i < list->attribute_count; i++) {
         if(list->attributes[i].consumed || list->attributes[i].kind != kind) continue;
+        list->attributes[i].consumed = true;
         return &list->attributes[i];
     }
     return NULL;
@@ -17,4 +19,9 @@ hlir_attribute_t *hlir_attribute_find(hlir_attribute_list_t *list, hlir_attribut
 
 void hlir_attribute_list_add_packed(hlir_attribute_list_t *list, source_location_t source_location) {
     list_add(list, HLIR_ATTRIBUTE_KIND_PACKED, source_location);
+}
+
+void hlir_attribute_list_add_link(hlir_attribute_list_t *list, const char *name, source_location_t source_location) {
+    hlir_attribute_t *attr = list_add(list, HLIR_ATTRIBUTE_KIND_LINK, source_location);
+    attr->link.name = name;
 }
--- src/hlir/attribute.h
+++ src/hlir/attribute.h
@@ -7,13 +7,19 @@
 #define HLIR_ATTRIBUTE_LIST_INIT ((hlir_attribute_list_t) { .attribute_count = 0, .attributes = NULL })
 
 typedef enum {
-    HLIR_ATTRIBUTE_KIND_PACKED
+    HLIR_ATTRIBUTE_KIND_PACKED,
+    HLIR_ATTRIBUTE_KIND_LINK
 } hlir_attribute_kind_t;
 
 typedef struct {
     hlir_attribute_kind_t kind;
     source_location_t source_location;
     bool consumed;
+    union {
+        struct {
+            const char *name;
+        } link;
+    };
 } hlir_attribute_t;
 
 typedef struct {
@@ -24,3 +30,4 @@ typedef struct {
 hlir_attribute_t *hlir_attribute_find(hlir_attribute_list_t *list, hlir_attribute_kind_t kind);
 
 void hlir_attribute_list_add_packed(hlir_attribute_list_t *list, source_location_t source_location);
+void hlir_attribute_list_add_link(hlir_attribute_list_t *list, const char *name, source_location_t source_location);
