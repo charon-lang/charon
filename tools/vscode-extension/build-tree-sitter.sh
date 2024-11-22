dc50f81ab2fecb30fdbc2f303ba33a6261ca1996
WuX <wux@thenest.dev>
2024-11-23 00:26:03 +0200

    tools: rewrite the vscode-extension to use tree-sitter for highlighting... textmate is unbearable

---
+++ tools/vscode-extension/build-tree-sitter.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TREE_SITTER_PATH=../tree-sitter-charon
+
+(cd $TREE_SITTER_PATH && tree-sitter generate)
+(cd $TREE_SITTER_PATH &&  tree-sitter build --wasm)
+
+mkdir -p ./out
+cp $TREE_SITTER_PATH/tree-sitter-charon.wasm ./out
+cp $TREE_SITTER_PATH/queries/highlights.scm ./out
