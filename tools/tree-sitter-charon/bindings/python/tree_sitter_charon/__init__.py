bc0314ea097b8745f9008fb95373842906361328
WuX <wux@thenest.dev>
2024-11-23 00:26:46 +0200

    tools: add tree-sitter for charon

---
+++ tools/tree-sitter-charon/bindings/python/tree_sitter_charon/__init__.py
@@ -0,0 +1,5 @@
+"Charon grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ tools/tree-sitter-charon/bindings/python/tree_sitter_treesitter/__init__.py
@@ -0,0 +1,5 @@
+"Treesitter grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
