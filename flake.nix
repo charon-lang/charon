b4aaf2827fe1ab10039984d45b9f612ca48d5fd7
WuX <wux@thenest.dev>
2024-12-02 14:02:55 +0200

    tools: add package to nix flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
+    outputs = { self, nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
         let
             pkgs = import nixpkgs { inherit system; };
             inherit (pkgs) lib stdenv mkShell fetchFromGitLab buildGoModule;
@@ -26,6 +26,25 @@
                     emscripten
                     valgrind
                 ];
+            };
+
+            defaultPackage = stdenv.mkDerivation rec {
+                name = "charon";
+                src = self;
+                
+                nativeBuildInputs = with pkgs; [ clang_18 clang-tools_18 ];
+                buildInputs = with pkgs; [ pcre2 llvmPackages_18.libllvm ];
+
+                installPhase = ''
+                    mkdir -p $out/bin
+                    cp charon $out/bin/
+                '';
+                
+                meta = {
+                    description = "Based programming language.";
+                    homepage = https://git.thenest.dev/wux/charon;
+                    maintainers = with lib.maintainers; [ wux ];
+                };
             };
         }
     );
