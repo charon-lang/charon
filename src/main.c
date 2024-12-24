95c69e0bda74fb75fd0738a854aa33401642734d
WuX <wux@thenest.dev>
2024-12-24 08:12:02 +0200

    main: cleaned up

--- src/main.c
+++ src/main.c
@@ -1,56 +1,27 @@
 #include "lib/source.h"
+#include "lib/memory.h"
 #include "lib/log.h"
-#include "lib/alloc.h"
+#include "ast/node.h"
 #include "lexer/tokenizer.h"
 #include "parser/parser.h"
 #include "pass/pass.h"
+#include "codegen/codegen.h"
 
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
-#include <libgen.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <llvm-c/Core.h>
-#include <llvm-c/TargetMachine.h>
 
 #define SEMVER_PRE_RELEASE "alpha.4.1"
 #define SEMVER_MAJOR 1
 #define SEMVER_MINOR 0
 #define SEMVER_PATCH 0
 
-#define VALID_MEMORY_MODELS "default, kernel, tiny, small, medium, large"
-
-#define BASH_COLOR(COLOR, TEXT) "\e[" COLOR "m" TEXT "\e[0m"
-
-// static char *find_extension(char *path, char extension_separator, char path_separator) {
-//     if(path == NULL) return NULL;
-
-//     char *last_extension = strrchr(path, extension_separator);
-//     char *last_path = (path_separator == '\0') ? NULL : strrchr(path, path_separator);
-//     if(last_extension == NULL || last_path >= last_extension) return NULL;
-
-//     return last_extension;
-// }
-
-// static char *strip_extension(char *path, char extension_separator, char path_separator) {
-//     if(path == NULL) return NULL;
-
-//     char *extension = find_extension(path, extension_separator, path_separator);
-//     if(extension == NULL) return strdup(path);
-
-//     size_t new_length = (uintptr_t) extension - (uintptr_t) path;
-//     char *new = malloc(new_length + 1);
-//     memcpy(new, path, new_length);
-//     new[new_length] = '\0';
-//     return new;
-// }
+#define C(COLOR, TEXT) "\e[" COLOR "m" TEXT "\e[0m"
 
 static char *add_extension(const char *path, char *extension, char extension_separator) {
     if(path == NULL) return NULL;
-
     size_t path_length = strlen(path);
     size_t extension_length = strlen(extension);
     char *new = malloc(path_length + 1 + extension_length + 1);
@@ -61,7 +32,7 @@ static char *add_extension(const char *path, char *extension, char extension_sep
     return new;
 }
 
-static char *strdup_basename(char *path) {
+static char *strdup_basename(const char *path) {
     char *original = strdup(path);
     char *temporary = basename(original);
     char *new = strdup(temporary);
@@ -69,135 +40,94 @@ static char *strdup_basename(char *path) {
     return new;
 }
 
-static void compile(source_t **sources, size_t source_count, const char *dest_path, bool ir, LLVMCodeModel code_model, const char *optstr);
+int main(int argc, const char *argv[]) {
+    enum { COMPILE, RUN } mode;
 
-int main(int argc, char *argv[]) {
-    enum { DEFAULT, COMPILE, HELP } mode = DEFAULT;
-    enum { OBJECT, IR } format = OBJECT;
-    enum { NONE, O1, O2, O3 } optimization = O1;
+    if(argc < 2) log_fatal("missing subcommand");
 
-    LLVMCodeModel code_model = LLVMCodeModelDefault;
+    if(strcasecmp(argv[1], "info") == 0) {
+        printf("Charon %u.%u.%u%s\n", SEMVER_MAJOR, SEMVER_MINOR, SEMVER_PATCH, SEMVER_PRE_RELEASE == NULL ? "" : "-" SEMVER_PRE_RELEASE);
+        printf("Built at " __DATE__ " " __TIME__ "\n");
+        return EXIT_SUCCESS;
+    } else if(strcasecmp(argv[1], "compile") == 0) mode = COMPILE;
+    else if(strcasecmp(argv[1], "run") == 0) mode = RUN;
+    else log_fatal("invalid subcommand");
+
+    size_t source_count = 0;
+    source_t **sources = NULL;
 
     const char *dest_path = NULL;
+    bool emit_ir = false;
+    codegen_code_model_t code_model = CODEGEN_CODE_MODEL_DEFAULT;
+    codegen_optimization_t optimization = mode == CODEGEN_OPTIMIZATION_O1;
+    for(int i = 2; i < argc; i++) {
+        if(argv[i][0] != '-') {
+            char *name = strdup_basename(argv[i]);
 
-    while(true) {
-        int option_index = 0;
-        int option = getopt_long(argc, argv, "ci", (struct option[]) {
-            { .name = "help", .has_arg = no_argument, .val = 'h' },
-            { .name = "compile", .has_arg = no_argument, .val = 'c' },
+            FILE *file = fopen(argv[i], "r");
+            if(file == NULL) log_fatal("open source file '%s' (%s)", name, strerror(errno));
 
-            { .name = "o", .has_arg = required_argument, .val = 'o' },
+            struct stat st;
+            if(fstat(fileno(file), &st) != 0) log_fatal("stat source file '%s' (%s)", name, strerror(errno));
 
-            { .name = "llvm-ir", .has_arg = no_argument, .val = IR, .flag = (int *) &format },
-            { .name = "memory-model", .has_arg = required_argument, .val = 'm' },
+            void *data = malloc(st.st_size);
+            if(fread(data, 1, st.st_size, file) != st.st_size) log_fatal("read source file '%s' (%s)", name, strerror(errno));
+            if(fclose(file) != 0) log_warning("close source file '%s' (%s)", name, strerror(errno));
 
-            { .name = "O0", .has_arg = no_argument, .val = NONE, .flag = (int *) &optimization },
-            { .name = "O1", .has_arg = no_argument, .val = O1, .flag = (int *) &optimization },
-            { .name = "O2", .has_arg = no_argument, .val = O2, .flag = (int *) &optimization },
-            { .name = "O3", .has_arg = no_argument, .val = O3, .flag = (int *) &optimization },
-            {}
-        }, &option_index);
-        if(option == -1) break;
-
-        switch(option) {
-            case 'h': mode = HELP; break;
-            case 'c': mode = COMPILE; break;
-            case 'm':
-                if(strcmp(optarg, "default") == 0) code_model = LLVMCodeModelDefault;
-                else if(strcmp(optarg, "kernel") == 0) code_model = LLVMCodeModelKernel;
-                else if(strcmp(optarg, "tiny") == 0) code_model = LLVMCodeModelTiny;
-                else if(strcmp(optarg, "small") == 0) code_model = LLVMCodeModelSmall;
-                else if(strcmp(optarg, "medium") == 0) code_model = LLVMCodeModelMedium;
-                else if(strcmp(optarg, "large") == 0) code_model = LLVMCodeModelLarge;
-                else log_fatal("unknown memory model `%s` (Use one of: " VALID_MEMORY_MODELS ")", optarg);
-                break;
-            case 'o':
-                dest_path = strdup(optarg);
-                break;
+            sources = reallocarray(sources, ++source_count, sizeof(source_t *));
+            sources[source_count - 1] = source_make(name, data, st.st_size);
+            continue;
         }
+
+        if(strcmp(argv[i], "-O0") == 0) { optimization = CODEGEN_OPTIMIZATION_NONE; continue; }
+        if(strcmp(argv[i], "-O1") == 0) { optimization = CODEGEN_OPTIMIZATION_O1; continue; }
+        if(strcmp(argv[i], "-O2") == 0) { optimization = CODEGEN_OPTIMIZATION_O2; continue; }
+        if(strcmp(argv[i], "-O3") == 0) { optimization = CODEGEN_OPTIMIZATION_O3; continue; }
+
+        if(strncasecmp(argv[i], "--optimization=", 15) == 0) {
+            char c = argv[i][15];
+            switch(c) {
+                case '0': optimization = CODEGEN_OPTIMIZATION_NONE; break;
+                case '1': optimization = CODEGEN_OPTIMIZATION_O1; break;
+                case '2': optimization = CODEGEN_OPTIMIZATION_O2; break;
+                case '3': optimization = CODEGEN_OPTIMIZATION_O3; break;
+                default: goto invalid_optimization;
+            }
+            if(argv[i][16] != '\0') {
+                invalid_optimization:
+                log_warning("invalid optimization level '%s'", &argv[i][15]);
+            }
+            continue;
+        }
+
+        if(mode == COMPILE && strncasecmp(argv[i], "--code-model=", 13) == 0) {
+            const char *left = &argv[i][13];
+            if(strcasecmp(left, "default") == 0) code_model = CODEGEN_CODE_MODEL_DEFAULT;
+            else if(strcasecmp(left, "kernel") == 0) code_model = CODEGEN_CODE_MODEL_KERNEL;
+            else if(strcasecmp(left, "tiny") == 0) code_model = CODEGEN_CODE_MODEL_TINY;
+            else if(strcasecmp(left, "small") == 0) code_model = CODEGEN_CODE_MODEL_SMALL;
+            else if(strcasecmp(left, "medium") == 0) code_model = CODEGEN_CODE_MODEL_MEDIUM;
+            else if(strcasecmp(left, "large") == 0) code_model = CODEGEN_CODE_MODEL_LARGE;
+            else log_warning("invalid code model '%s', ignoring", left);
+            continue;
+        }
+
+        if(mode == COMPILE && strncasecmp(argv[i], "--dest=", 7) == 0) { dest_path = &argv[i][7]; continue; }
+
+        if(mode == COMPILE && strcasecmp(argv[i], "--emit-ir") == 0) { emit_ir = true; continue; }
+
+        log_warning("unrecognized flag '%s'", argv[i]);
     }
 
-    const char *optstr = NULL;
-    switch(optimization) {
-        case NONE: break;
-        case O1: optstr = "default<O1>"; break;
-        case O2: optstr = "default<O2>"; break;
-        case O3: optstr = "default<O3>"; break;
+    if(source_count == 0) {
+        log_warning("no sources");
+        return EXIT_SUCCESS;
     }
 
-    switch(mode) {
-        case HELP: {
-            printf("Charon %u.%u.%u%s%s%s\n", SEMVER_MAJOR, SEMVER_MINOR, SEMVER_PATCH, SEMVER_PRE_RELEASE == NULL ? "" : "-", SEMVER_PRE_RELEASE, LLVMIsMultithreaded() ? " (multithreaded)" : "");
-            printf("Built at " __DATE__ " " __TIME__ "\n");
-            printf(
-                "Usage: charon "
-                "[" BASH_COLOR("1", "--help") "|" BASH_COLOR("1", "--compile") "] "
-                "[" BASH_COLOR("1", "-O") BASH_COLOR("4", "level") "] "
-                "[" BASH_COLOR("1", "--llvm-ir") "] "
-                "[" BASH_COLOR("1", "--memory-model") "=" BASH_COLOR("4", "model") "] "
-                BASH_COLOR("4", "files") "...\n"
-            );
-            printf("\n");
-            printf(
-                "Options:\n"
-                "  " BASH_COLOR("1", "--help") ", " BASH_COLOR("1", "--compile") "\n"
-                "      Select operation, " BASH_COLOR("4", "compile") " is the default mode.\n"
-                "  " BASH_COLOR("1", "--llvm-ir") "\n"
-                "      Compile to LLVM IR.\n"
-                "  " BASH_COLOR("1", "--memory-model") "=" BASH_COLOR("4", "model") "\n"
-                "      Code model to use. Choose one of: " VALID_MEMORY_MODELS ".\n"
-                "  " BASH_COLOR("1", "-O") BASH_COLOR("4", "level") "\n"
-                "      Optimization level. Valid levels are: 0, 1, 2, 3.\n"
-            );
-        } break;
-        case DEFAULT:
-        case COMPILE:
-            size_t count = argc - optind;
-            source_t **sources = reallocarray(NULL, count, sizeof(source_t *));
-            for(int i = optind, j = 0; i < argc; i++, j++) {
-                char *name = strdup_basename(argv[i]);
-
-                FILE *file = fopen(argv[i], "r");
-                if(file == NULL) log_fatal("open source file '%s' (%s)", name, strerror(errno));
-
-                struct stat st;
-                if(fstat(fileno(file), &st) != 0) log_fatal("stat source file '%s' (%s)", name, strerror(errno));
-
-                void *data = malloc(st.st_size);
-                if(fread(data, 1, st.st_size, file) != st.st_size) log_fatal("read source file '%s' (%s)", name, strerror(errno));
-                if(fclose(file) != 0) log_warning("close source file '%s' (%s)", name, strerror(errno));
-
-                sources[j] = source_make(name, data, st.st_size);
-            }
-
-            if(count == 0) {
-                log_warning("No sources provided\n");
-                break;
-            }
-
-            if(dest_path == NULL) {
-                if(count > 0) {
-                    dest_path = "out";
-                } else {
-                    dest_path = sources[0]->name;
-                }
-                dest_path = add_extension(dest_path, format == IR ? "ll" : "o", '.');
-            }
-
-            compile(sources, count, dest_path, format == IR, code_model, optstr);
-
-            for(size_t i = 0; i < count; i++) source_free(sources[i]);
-            free((char *) dest_path);
-            free(sources);
-            break;
-    }
-    return EXIT_SUCCESS;
-}
-
-static void compile(source_t **sources, size_t source_count, const char *dest_path, bool ir, LLVMCodeModel code_model, const char *optstr) {
-    ast_node_t *ast_root_nodes[source_count];
+    if(mode == RUN && optimization == CODEGEN_OPTIMIZATION_NONE) log_warning("run mode is unstable with zero optimizations");
 
     /* Parse: Source -> Tokens -> AST */
+    ast_node_t *ast_root_nodes[source_count];
     memory_allocator_t *ast_allocator = memory_allocator_make();
     memory_active_allocator_set(ast_allocator);
     for(size_t i = 0; i < source_count; i++) {
@@ -227,13 +157,23 @@ static void compile(source_t **sources, size_t source_count, const char *dest_pa
     /* Passes: IR -> IR */
     pass_eval_types(&unit, type_cache);
 
-    /* Codegen: IR -> Binary */
-    if(ir) {
-        pass_codegen_ir(&unit, type_cache, dest_path);
-    } else {
-        pass_codegen(&unit, type_cache, dest_path, optstr, code_model);
+    /* Codegen: IR -> LLVM_IR */
+    codegen_context_t *cg_context = codegen(&unit, type_cache, optimization, code_model);
+
+    int ret = EXIT_SUCCESS;
+    switch(mode) {
+        case COMPILE: {
+            if(dest_path == NULL) dest_path = add_extension(source_count > 0 ? "out" : sources[0]->name, emit_ir ? "ll" : "o", '.');
+            codegen_emit(cg_context, dest_path, emit_ir);
+            break;
+        }
+        case RUN: ret = codegen_run(cg_context); break;
     }
 
+    /* Cleanup */
+    codegen_dispose_context(cg_context);
     memory_active_allocator_set(NULL);
     memory_allocator_free(ir_allocator);
+
+    return ret;
 }
