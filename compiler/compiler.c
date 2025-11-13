ffee5a41f65a56f71522a7c8936cf3d4dedca319
WuX <wux@thenest.dev>
2025-11-13 23:05:11 +0200

    feat(compiler): align with utf8 and diagnostic changes made to lib

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -1,3 +1,7 @@
+#include "charon/diag.h"
+#include "charon/path.h"
+#include "charon/utf8.h"
+
 #include <assert.h>
 #include <charon/element.h>
 #include <charon/lexer.h>
@@ -30,13 +34,14 @@ static void print_tree(charon_memory_allocator_t *allocator, charon_element_t *e
         case CHARON_ELEMENT_TYPE_TOKEN:
             charon_token_kind_t token_kind = charon_element_token_kind(element->inner);
             const char *kind_text = charon_token_kind_tostring(token_kind);
-            const char *text = charon_element_token_text(element->inner);
+            const charon_utf8_text_t *token_text = charon_element_token_text(element->inner);
+            const char *str = token_text == nullptr ? nullptr : charon_utf8_as_string(token_text);
             printf("Token(");
-            if(text == nullptr || strcmp(kind_text, text) != 0) {
+            if(str == nullptr || strcmp(kind_text, str) != 0) {
                 printf("%s", kind_text);
-                if(text != nullptr) printf(" ");
+                if(str != nullptr) printf(" ");
             }
-            if(text != nullptr) printf("`%s`", text);
+            if(str != nullptr) printf("`%s`", str);
             printf(")\n");
             break;
     }
@@ -62,9 +67,9 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    size_t text_length = st.st_size;
-    char *text = malloc(text_length);
-    if(fread(text, 1, text_length, file) != text_length) {
+    size_t data_size = st.st_size;
+    void *data = malloc(data_size);
+    if(fread(data, 1, data_size, file) != data_size) {
         printf("read source file '%s' (%s)\n", name, strerror(errno));
         exit(EXIT_FAILURE);
     }
@@ -75,17 +80,41 @@ int main(int argc, char **argv) {
     charon_element_cache_t *cache = charon_element_cache_make(allocator);
 
     // Parse
-    charon_lexer_t *lexer = charon_lexer_make(cache, text, text_length);
+    charon_utf8_text_t *text = charon_utf8_from(data, data_size);
+    charon_lexer_t *lexer = charon_lexer_make(cache, text);
     charon_parser_t *parser = charon_parser_make(cache, lexer);
 
-    const charon_element_inner_t *root_element = charon_parser_parse_root(parser);
+    charon_parser_output_t parser_output = charon_parser_parse_root(parser);
 
-    print_tree(allocator, charon_element_wrap_root(allocator, root_element), 0);
+    charon_element_t *root_element = charon_element_wrap_root(allocator, parser_output.root);
+    print_tree(allocator, root_element, 0);
+
+    charon_diag_item_t *next_diag = parser_output.diagnostics;
+    while(next_diag != nullptr) {
+        charon_diag_item_t *diag = next_diag;
+        next_diag = diag->next;
+
+        charon_element_t *current_element = root_element;
+        for(size_t i = 0; i < diag->path->length; i++) {
+            size_t child_index = diag->path->steps[i];
+            assert(charon_element_type(current_element->inner) == CHARON_ELEMENT_TYPE_NODE);
+            assert(charon_element_node_child_count(current_element->inner) > child_index);
+
+            current_element = charon_element_wrap_node_child(allocator, current_element, child_index);
+        }
+
+        printf("DIAGNOSTIC %s\n", charon_diag_tostring(diag->kind));
+        print_tree(allocator, current_element, 0);
+
+        charon_path_destroy(diag->path);
+        free(diag);
+    }
 
     charon_lexer_destroy(lexer);
     charon_parser_destroy(parser);
 
     free(text);
+    free(data);
 
     // Cleanup project context
     charon_element_cache_destroy(cache);
