c22c8aaea778f49ed4f67d8f53d3dd9d7c95badd
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2025-12-14 14:26:21 +1100

    tests: add tests (todo rename them)

---
+++ tests/test.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env sh
+
+MODE="all"
+
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -a|--all)
+            MODE="all"
+            shift
+            ;;
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1")
+            shift
+            ;;
+    esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}"
+
+print_result() {
+    echo -n "| "
+    if [ $1 -eq 0 ]; then
+        echo -ne "\e[41;30mFAIL"
+    else
+        echo -ne "\e[42;30mPASS"
+    fi
+    echo -ne "\e[0m"
+    echo -n " $2"
+    if [ $# -eq 3 ]; then
+        echo -n " ($3)"
+    fi
+    echo -ne "\n"
+}
+
+PASSED=0
+FAILED=0
+TOTAL=0
+run_exec() {
+    TEST_PATH="${1%.test}"
+    TEST_NAME="${TEST_PATH##*/}"
+    TOTAL=$(($TOTAL+1))
+    CHARON_FILE="$TEST_PATH.charon"
+    if ! [ -f "$CHARON_FILE" ]; then
+        print_result 0 $TEST_NAME "Missing $TEST_NAME.charon file"
+        return
+    fi
+
+    ./build/compiler/charonc $CHARON_FILE --shut-the-fuck-up >> $TEST_PATH.result
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+        print_result 0 $TEST_NAME "tester exited with $EXIT_CODE"
+        return
+    fi
+
+    RESULT="$(cat $TEST_PATH.result)"
+    EXPECTED="$(cat $1)"
+    if [[ "$RESULT" == "$EXPECTED" ]]; then
+        print_result 1 $TEST_NAME
+        PASSED=$(($PASSED+1))
+        rm -f $TEST_PATH.result
+    else
+        print_result 0 $TEST_NAME
+        FAILED=$(($FAILED+1))
+        diff --color <(echo "$RESULT") <(echo "$EXPECTED")
+    fi
+}
+
+case $MODE in
+    all)
+        echo "| Running Execution Tests"
+        for TEST_FILE in tests/exec/*.test; do run_exec $TEST_FILE; done
+        echo "| Done $PASSED/$TOTAL Passed ($FAILED Failed)"
+        ;;
+esac
